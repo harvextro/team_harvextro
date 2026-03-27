19ee82bcc26f6373d103d62881ccf2f836e6145b
Dilni_Wijesinghe <rohansiwije@gmail.com>
2026-03-28 04:59:43 +0530

    Update data_prep.py

--- Object_Detection_model/data_prep.py
+++ Object_Detection_model/data_prep.py
@@ -2,3 +2,5 @@
 # Kaggle: /kaggle/input/<your-dataset-name>/Data_Set
 DATASET_ROOT = "/kaggle/input/datasets/dilniwijesinghe/scotch-bonnet-dataset/Data_Set"
 OUTPUT_ROOT  = "/kaggle/working/harvextro_yolov8"
+
+CLASSES = ["green_chili", "red_chili", "yellow_chili"]  # must match _classes.txt order
