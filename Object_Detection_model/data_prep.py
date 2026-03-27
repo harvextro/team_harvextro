e4f34e5c1f6de77e5fd6e9aae3d300c9ef76a974
Dilni_Wijesinghe <rohansiwije@gmail.com>
2026-03-28 04:59:22 +0530

    Create data_prep.py

---
+++ Object_Detection_model/data_prep.py
@@ -0,0 +1,4 @@
+# ─── CONFIGURE PATHS ────────────────────────────────────────────
+# Kaggle: /kaggle/input/<your-dataset-name>/Data_Set
+DATASET_ROOT = "/kaggle/input/datasets/dilniwijesinghe/scotch-bonnet-dataset/Data_Set"
+OUTPUT_ROOT  = "/kaggle/working/harvextro_yolov8"
